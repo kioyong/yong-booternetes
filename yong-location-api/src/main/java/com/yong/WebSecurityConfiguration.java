532495bbb839e8e0ddd1489ba98ce4dc3ad72d25
LiangYong <yong.a.liang@accenture.com>
2020-08-05 16:22:37 +0800

    fix spring version issue

--- yong-location-api/src/main/java/com/yong/WebSecurityConfiguration.java
+++ yong-location-api/src/main/java/com/yong/WebSecurityConfiguration.java
@@ -1,15 +1,8 @@
 package com.yong;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
 
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -17,19 +10,19 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .authorizeRequests(authorizeRequests ->
-                authorizeRequests
-                    .anyRequest().authenticated()
-            ).oauth2ResourceServer(oauth2ResourceServer ->
-            oauth2ResourceServer
-                .jwt(jwt ->
-                    jwt.decoder(jwtDecoder())
-                )
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .anyRequest().authenticated()
+                ).oauth2ResourceServer(oauth2ResourceServer ->
+                        oauth2ResourceServer.jwt().jwkSetUri("http://localhost:8087/.well-known/jwks.json")
+//                .jwt(jwt ->
+//                    jwt.decoder(jwtDecoder())
+//                )
         );
     }
 
-    @Bean
-    JwtDecoder jwtDecoder() {
+//    @Bean
+//    JwtDecoder jwtDecoder() {
 //        JWSKeySelector<SecurityContext> jwsKeySelector =
 //            new SingleKeyJWSKeySelector<>(this.jwsAlgorithm, this.secretKey);
 //        DefaultJWTProcessor<SecurityContext> jwtProcessor = new DefaultJWTProcessor<>();
@@ -38,10 +31,10 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 //        // Spring Security validates the claim set independent from Nimbus
 //        jwtProcessor.setJWTClaimsSetVerifier((claims, context) -> { });
 
-        SecretKey secretKey = ((SecretKey) (new SecretKeySpec("mcegemsverysecret".getBytes(), "HMACSHA256")));
-        return NimbusJwtDecoder.withSecretKey(secretKey).macAlgorithm(MacAlgorithm.HS256).build();
+//        SecretKey secretKey = ((SecretKey) (new SecretKeySpec("mcegemsverysecret".getBytes(), "HMACSHA256")));
+//        return NimbusJwtDecoder.withSecretKey(secretKey).macAlgorithm(MacAlgorithm.HS256).build();
 //        return new NimbusJwtDecoder(withoutSigning());
-    }
+}
 
 //    private static JWTProcessor<SecurityContext> withoutSigning() {
 //        return new MockJwtProcessor();
@@ -59,4 +52,4 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 //            }
 //        }
 //    }
-}
+//}
--- yong-location-api/src/main/java/com/yong/YongLocationApiApplication.java
+++ yong-location-api/src/main/java/com/yong/YongLocationApiApplication.java
@@ -17,6 +17,7 @@ public class YongLocationApiApplication {
 
 	@GetMapping("/hello")
 	public String hello(){
+
 		return "hello1";
 	}
 }
